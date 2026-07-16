97825963007c793915b3f92c3fa08a0b6feaee2a
Arunesh <47033504+AruneshDev@users.noreply.github.com>
2026-07-15 21:56:29 -0400

    Add: anagram-groups - submission-10

---
+++ Data Structures & Algorithms/anagram-groups/submission-10.py
@@ -0,0 +1,30 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # intuition 
+        # use letters of strs as key so any other word containing them in the same freq 
+        # can be grouped together
+        # using hash tables
+        res=defaultdict(list)
+        for s in strs:
+            count=[0]*26
+            for c in s:
+                count[ord(c)-ord('a')]+=1
+            res[tuple(count)].append(s)
+        return list(res.values())
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
